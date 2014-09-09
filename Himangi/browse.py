8f08fb95775a9e8c420060bc3167ad465ed8678b
shobasri <shobasrinath.s@gmail.com>
2014-09-08 22:01:48 -0700

    test comment

--- Himangi/browse.py
+++ Himangi/browse.py
@@ -3,9 +3,3 @@ from selenium import webdriver #some more changes
 
 br = webdriver.Firefox()
 br.get("http://cadence.com")
-myList = br.find_elements_by_tag_name("div")
-#elem = br.find_div("div")
-#print elem
-print len(myList)
-print myList[100].id
-
