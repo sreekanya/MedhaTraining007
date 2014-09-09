716c49beabdd7e48dd02c94fafa8a76bc488dedb
H78 <himangisathaye@gmail.com>
2014-09-08 20:53:51 -0700

    Addition
    Addition

---
+++ Himangi/Assignments/pmi.py
@@ -0,0 +1,17 @@
+import math
+loanAmt = raw_input("please enter the amount of loan: ")
+loanAmt = int(loanAmt)
+rate = raw_input("please enter the rate of interest: ")
+rate = float(rate)
+#y = int(rate)
+duration = raw_input("please enter the no. of months to pay off: ")
+duration = int(duration)
+
+def calculate(loanAmt,rate,duration):
+    r = rate/12/100
+    x = math.pow(duration,1+r) 
+    emi = int(loanAmt)* rate*x/x-1
+    print emi
+
+calculate(loanAmt,rate,duration)
+
