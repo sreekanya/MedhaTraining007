05cfb259395c5ae11cd69d0bc97478dbd29b92d6
geeta <appigeeta@gmail.com>
2014-09-11 12:14:21 -0700

    check valid email

---
+++ geeta/re-assignments/email.py
@@ -0,0 +1,9 @@
+# check given email address is valid or no.
+import re
+
+input1=raw_input("please enter the email address:")
+pt=re.compile('[a-z]+\.[A-Z]+\.[0-9]+@[a-z]+.com$')
+
+result=pt.match(input1)
+print result
+#script not working
