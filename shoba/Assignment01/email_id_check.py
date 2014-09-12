64dbda64e09ca0a24c4fec6760d5e6d20f1a4174
naninice2000 <naninice2000@gmail.com>
2014-09-11 21:37:41 -0700

    corrected patern

--- shoba/Assignment01/email_id_check.py
+++ shoba/Assignment01/email_id_check.py
@@ -1,25 +1,22 @@
 #python script to check if entered email id is valid or not
 import re
 #accepting email id from user
+
+
 emailAddress = (raw_input("enter a  email id")
-pt=re.compile('^[a-z]+\@+[a-z]+\.[com]')
-check1 = pt.match(emailAddress)
-pt2=re.compile('[a-z]+ [0-9]\@+[a-z]+\.[com]')
-check2 = pt2.match(emailAddress)
-pt3=re.compile('[0-9]+[a-z]+[0-9]@+[a-z]+\.[com]')
-check3 = pt3.match(emailAddress)
-pt4=re.compile('[a-z]+ [0-9]+[a-z]+\@+[a-z]+\.[com]')
+
+pt4=re.compile('[a-z]+[0-9]*[a-z]*@[a-z]+\.com$')  ### shoba12srinath@gmail.com or shoba12@gmail.com or shoba@yahoo.com 
+
+## shoba.srinath@gmail.com
+## shoba_srinath@gmail.com
+## shoba.srinath2003@gmail.com
+## shoba_srinath2003@gmail.com
+
+
 check4 = pt4.match(emailAddress)
-if check1 != none:
-	   print "is a valid email id"
-elif check2 != none:
-	  print "is a valid email id"
-elif check3 ! = none
-	  print "is a valid email id"	
-elif check3 ! = none
-	  print "is a valid email id"
- else:
-		print "invalid email id"	
+
+if(check4 != None):
+	print emailAddress," is a valid email adddress"
 
 
 
