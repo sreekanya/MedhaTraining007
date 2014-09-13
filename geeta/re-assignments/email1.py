b0d13d401467acaccffed8cc0a99251ef2513346
geeta <appigeeta@gmail.com>
2014-09-13 10:11:49 -0700

    trial script for txt and py files

--- geeta/re-assignments/email1.py
+++ geeta/re-assignments/email1.py
@@ -2,7 +2,7 @@
 
 import re
 
-input1=raw_input("pleas enter email addrres")
+input1=raw_input("pleas enter email addrres:")
 pt= re.compile('^[a-z|A-Z]+\w+_.\w@[a-z]+.["com"|"biz"|"org"|"edu"|"co"|"au"|"uk"]+$')
 result=pt.match(input1)
 
