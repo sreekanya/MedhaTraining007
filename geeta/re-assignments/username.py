9f9157b67bc86650200faf26392665c2d26e8e72
geeta <appigeeta@gmail.com>
2014-09-12 17:29:00 -0700

    correcting the script

--- geeta/re-assignments/username.py
+++ geeta/re-assignments/username.py
@@ -1,11 +1,16 @@
+
 # check entered username is valid or not
 
 import re
 
 input1=raw_input("pls enter the user name you want to check:")
 
-pt=re.compile('^[a-z]+\[A-Z]+\[0-9]+')
+
+pt=re.compile('^[a-z|A-Z+a-z|A-Z|0-9\._-]+$')
 
 result=pt.match(input1)
 
-print result
+if (result !=None):
+	print input1,"is a valid username"
+else:
+	"invalid user name"
