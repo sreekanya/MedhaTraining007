d5d448af843149514cd6736bae91f283e0374b44
geeta <appigeeta@gmail.com>
2014-09-14 09:57:30 -0700

    tring to fix the script

---
+++ geeta/re-assignments/workingfile.py
@@ -0,0 +1,12 @@
+# to check file is text file or python file.
+
+import re
+input1=raw_input("please enter the file name:")
+
+pt=re.compile('^[a-z|A-Z]+\w*\.(txt|py)$')
+
+result=pt.match(input1)
+
+print result
+
+# script not working..............
