997aa5ef7715a52d526bcc9cd686b02745e81e67
geeta <appigeeta@gmail.com>
2014-09-16 20:44:39 -0700

    updated

--- geeta/re-assignments/validphno.py
+++ geeta/re-assignments/validphno.py
@@ -1,11 +1,11 @@
 # script for checking valid US phone number
 import re
 
-pt=re.compile('[1-9][0-9][0-9]\-[0-9][0-9][0-9]\-[0-9][0-9][0-9][0-9]$')
+pt=re.compile('[1-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$')
 
 input1=raw_input("Please enter the phone number:")
 print input1
 result=pt.match(input1)
 
 print result
-#it ia taking phone number with - inbetn,after escape sign also not working for without-
+#it not working for without-
