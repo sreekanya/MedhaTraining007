5b3326b8bfce0a712eba90b92d1b7fe82f871688
geeta <appigeeta@gmail.com>
2014-09-18 10:59:02 -0700

    csvelements

---
+++ geeta/selenium/csvelem.py
@@ -0,0 +1,23 @@
+# using list finding element in yahoo login
+
+fh=open ("csvelement.txt",'r')
+
+for i in fh:
+	newi=i.rstrip()
+	data=newi.split(',')
+	print data[0],data[1]
+	
+	
+	
+	
+	
+	
+	
+
+
+	
+
+
+
+
+
