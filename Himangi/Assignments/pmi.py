3a895deb3859ce9649bceecd555266a800d10cd7
H78 <himangisathaye@gmail.com>
2014-09-20 11:52:10 -0700

    test

--- Himangi/Assignments/pmi.py
+++ Himangi/Assignments/pmi.py
@@ -1,20 +1,18 @@
-# this script is to calcualte auto loan money payment per month 
-
 import math
-
 loanAmt = raw_input("please enter the amount of loan: ")
 loanAmt = int(loanAmt)
 rate = raw_input("please enter the rate of interest: ")
 rate = float(rate)
 #y = int(rate)
-
 duration = raw_input("please enter the no. of months to pay off: ")
 duration = int(duration)
 
 def calculate(loanAmt,rate,duration):
-    r = rate/12/100
-    x = math.pow(duration,1+r) 
-    emi = int(loanAmt)* rate*x/x-1
+    r = (rate/100)/12
+    print r
+    x =(1+r)**duration 
+    emi = int(loanAmt)*(r*x /(x-1))
+    emi = math.ceil(emi)
     print emi
 
 calculate(loanAmt,rate,duration)
