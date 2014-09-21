a729d68f108656efc9fdad319a56b2694cc3d547
naninice2000 <naninice2000@gmail.com>
2014-09-21 09:58:05 -0700

    added comments

--- Venkata/pythonSeleniumWebdriverClass/Examples/testMouseOverExample.py
+++ Venkata/pythonSeleniumWebdriverClass/Examples/testMouseOverExample.py
@@ -1,15 +1,17 @@
 import selenium 
 from selenium import webdriver
+# this action chain import is required to use mouse over functionality.
 
 from selenium.webdriver.common.action_chains import ActionChains
 
 br = webdriver.Firefox()
 br.get("http://ubuntu.com")
+
 try:
 	cloudMenuElement = br.find_element_by_link_text("Cloud")
 except:
 	print "not able to find Menu item Cloud"
-else: 
+else: # Once after finding Element we are tring to move mouse to that element using ActionChain modules and functionality 
 	actions = ActionChains(br)
 	actions.move_to_element(cloudMenuElement)
 	actions.perform()
