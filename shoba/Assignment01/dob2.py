f49cfafc2a3ec346b905031fd304e1913df34ac6
naninice2000 <naninice2000@gmail.com>
2014-09-24 22:21:14 -0700

    fixed

--- shoba/Assignment01/dob2.py
+++ shoba/Assignment01/dob2.py
@@ -4,8 +4,10 @@ from datetime import date
 from datetime import datetime
 today = date.today()
 my_date = raw_input("Enter B'date in mm/dd/yyyy format:")
-b_date = datetime.strptime(my_date, '%m/%d/%Y')
-print "given date birth is : " ,b_date
+dateinfo = my_date.split('/')
+b_date=date(int(dateinfo[2]),int(dateinfo[0]),int(dateinfo[1])) #.format('YYYY-MM-DD')
+newdate = b_date.month
+print "given date birth is : " ,b_date," today is ",today
 if today == b_date:
 	print "its today"
 else:
