6277b41601f074e2a5545b4b7c04a91c53ae6b27
naninice2000 <naninice2000@gmail.com>
2014-11-16 15:19:15 -0800

    validating and getting command line inputs

---
+++ Venkata/RESTapitest/CommandLineInput.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python 
+
+import sys
+
+commandLineInputs = sys.argv
+expectedDataFileName = ""
+configFileName = ""
+
+
+def checkInputs(commandLineInputs):
+	if(len(commandLineInputs)<3):
+		print "please enter nessasary inputs.. existing the test....!!! "
+		sys.exit()
+	else:
+		print "processing your data and starting the test..."
+
+	for i in commandLineInputs:
+		inputs = i.split('=')
+		if(inputs[0]=="expecteddatafile"):
+			expectedDataFileName = inputs[1]
+		elif(inputs[0]=="config"):
+			configFileName = inputs[1]
+		else:
+			print "unknow parameter... "
+			sys.exit()
+
+
+
+checkInputs()
